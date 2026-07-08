770e6ab627e57135d39b8951db525738a0b87541
ZikraZana <monozikk13@gmail.com>
2026-07-08 11:01:33 +0700

    konfigurasi database dan layout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing_page');
+});
+
+Route::get('/devi', function () {
+    return view('peta_devi');
+});
+Route::get('/zia', function () {
+    return view('peta_zia');
+});
+Route::get('/zikra', function () {
+    return view('peta_zikra');
 });
