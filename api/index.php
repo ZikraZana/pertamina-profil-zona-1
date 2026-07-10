587a9262cef89e74a72c92e5a65669d8465bc0d5
ZikraZana <monozikk13@gmail.com>
2026-07-10 10:25:31 +0700

    fix vercel part 2

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Tentukan jalur absolut ke autoload dan bootstrap Laravel
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$handle = $app->make(Request::class);
+
+$response = $handle->handle(
+    $request = Request::capture()
+)->send();
+
+$handle->terminate($request, $response);
