321185ddf16518a88947abd4daa5789b4ab74db6
ZikraZana <monozikk13@gmail.com>
2026-07-10 14:02:28 +0700

    penyesuaian folder dan file

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing_page');
 });
-
-Route::get('/devi', function () {
-    return view('peta_devi');
-});
-Route::get('/zia', function () {
-    return view('peta_zia');
-});
-Route::get('/zikra', function () {
-    return view('peta_zikra');
-});
