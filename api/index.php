f9c0e0e18da596eafef282c297167426e5df3006
ZikraZana <monozikk13@gmail.com>
2026-07-10 10:35:33 +0700

    fix vercel part 4

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Arahkan ke file vendor dan bootstrap di folder root terluar
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$handle = $app->make(Request::class);
+
+$response = $handle->handle(
+    $request = Request::capture()
+)->send();
+
+$handle->terminate($request, $response);
