a4aa190419d6826a99018eb076cfa3e8d6b1764d
ZikraZana <monozikk13@gmail.com>
2026-07-10 11:01:14 +0700

    fix vercel part 7

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$handle = $app->make(Request::class);
-
-$response = $handle->handle(
-    $request = Request::capture()
-)->send();
-
-$handle->terminate($request, $response);
+require __DIR__.'/../public/index.php';
